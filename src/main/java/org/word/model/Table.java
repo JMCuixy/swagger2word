86a8c062ded50842787b4b51c4465e5752bd309e
Iven.han <Iven.han@eisoo.com>
2020-07-21 16:55:06 +0800

    解决初始化项目，依赖包不全导致不能运行问题

--- src/main/java/org/word/model/Table.java
+++ src/main/java/org/word/model/Table.java
@@ -10,7 +10,103 @@ import java.util.List;
 @Data
 public class Table {
 
-    /**
+    public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRequestForm() {
+		return requestForm;
+	}
+
+	public void setRequestForm(String requestForm) {
+		this.requestForm = requestForm;
+	}
+
+	public String getResponseForm() {
+		return responseForm;
+	}
+
+	public void setResponseForm(String responseForm) {
+		this.responseForm = responseForm;
+	}
+
+	public String getRequestType() {
+		return requestType;
+	}
+
+	public void setRequestType(String requestType) {
+		this.requestType = requestType;
+	}
+
+	public List<Request> getRequestList() {
+		return requestList;
+	}
+
+	public void setRequestList(List<Request> requestList) {
+		this.requestList = requestList;
+	}
+
+	public List<Response> getResponseList() {
+		return responseList;
+	}
+
+	public void setResponseList(List<Response> responseList) {
+		this.responseList = responseList;
+	}
+
+	public String getRequestParam() {
+		return requestParam;
+	}
+
+	public void setRequestParam(String requestParam) {
+		this.requestParam = requestParam;
+	}
+
+	public String getResponseParam() {
+		return responseParam;
+	}
+
+	public void setResponseParam(String responseParam) {
+		this.responseParam = responseParam;
+	}
+
+	public ModelAttr getModelAttr() {
+		return modelAttr;
+	}
+
+	public void setModelAttr(ModelAttr modelAttr) {
+		this.modelAttr = modelAttr;
+	}
+
+	/**
      * 大标题
      */
     private String title;
